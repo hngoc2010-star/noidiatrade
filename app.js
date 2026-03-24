11e487f8013406dafcda23a46cfbe1a15e8b541e
Jack <hngoc2010@gmail.com>
2026-03-24 20:43:20 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,61 @@
-function format(num){
-return Number(num).toLocaleString("en-US")
+function numberFormat(x){
+
+if(!x) return ""
+
+return Number(x).toLocaleString("en-US")
+
+}
+
+function cleanNumber(x){
+
+return Number(x.replace(/,/g,''))
+
 }
 
 function calculate(row){
 
-let qty=row.querySelector(".qty").value
-let exw=row.querySelector(".exw").value
-let logistics=row.querySelector(".logistics").value
-let sell=row.querySelector(".sell").value
+let qty=cleanNumber(row.querySelector(".qty").value)
 
-qty=Number(qty)
-exw=Number(exw)
-logistics=Number(logistics)
-sell=Number(sell)
+let exw=cleanNumber(row.querySelector(".exw").value)
+
+let logistics=cleanNumber(row.querySelector(".logistics").value)
+
+let sell=cleanNumber(row.querySelector(".sell").value)
 
 let totalExw=qty*exw
+
 let sellAmount=qty*sell
+
 let profit=sellAmount-totalExw-logistics
 
-row.querySelector(".totalExw").innerText=format(totalExw)
-row.querySelector(".sellAmount").innerText=format(sellAmount)
-row.querySelector(".profit").innerText=format(profit)
+row.querySelector(".totalExw").innerText=numberFormat(totalExw)
+
+row.querySelector(".sellAmount").innerText=numberFormat(sellAmount)
+
+row.querySelector(".profit").innerText=numberFormat(profit)
 
 }
 
 document.addEventListener("input",function(e){
 
+if(e.target.classList.contains("number")){
+
+let value=e.target.value.replace(/,/g,'')
+
+if(!isNaN(value) && value!==""){
+
+e.target.value=numberFormat(value)
+
+}
+
+}
+
 let row=e.target.closest("tr")
 
 if(row){
+
 calculate(row)
+
 }
 
 })
@@ -42,7 +68,8 @@ let newRow=tbody.rows[0].cloneNode(true)
 
 newRow.querySelectorAll("input").forEach(i=>i.value="")
 
-newRow.querySelectorAll(".totalExw,.sellAmount,.profit").forEach(i=>i.innerText="0")
+newRow.querySelectorAll(".totalExw,.sellAmount,.profit")
+.forEach(i=>i.innerText="0")
 
 tbody.appendChild(newRow)
 

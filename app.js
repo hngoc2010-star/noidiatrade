aa465150d823b5c9b95a6a581de44dede0b954bb
Jack <hngoc2010@gmail.com>
2026-03-24 21:53:44 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-let dsDon=[]
-
 function dinhDangSo(n){
 
 return Number(n).toLocaleString("en-US")
@@ -24,26 +22,33 @@ o.value=dinhDangSo(v)
 
 }
 
+function formatDateVN(date){
+
+if(!date) return ""
+
+let d=new Date(date)
+
+let day=String(d.getDate()).padStart(2,'0')
+let month=String(d.getMonth()+1).padStart(2,'0')
+let year=d.getFullYear()
+
+return `${day}/${month}/${year}`
+
+}
+
 function tinh(box){
 
-let soLuong=boDauPhay(box.querySelector(".soluong").value)
-
+let qty=boDauPhay(box.querySelector(".soluong").value)
 let exw=boDauPhay(box.querySelector(".exw").value)
-
 let log=boDauPhay(box.querySelector(".log").value)
-
 let ban=boDauPhay(box.querySelector(".ban").value)
 
-let tong=soLuong*exw
-
-let doanhThu=soLuong*ban
-
+let tong=qty*exw
+let doanhThu=qty*ban
 let loiNhuan=doanhThu-tong-log
 
 box.querySelector(".tong").innerText=dinhDangSo(tong)
-
 box.querySelector(".doanhthu").innerText=dinhDangSo(doanhThu)
-
 box.querySelector(".loinhuan").innerText=dinhDangSo(loiNhuan)
 
 capNhatTong()
@@ -67,7 +72,6 @@ document.getElementById("tongLoiNhuan").innerText=dinhDangSo(tong)
 function taoDon(data={}){
 
 let div=document.createElement("div")
-
 div.className="order"
 
 div.innerHTML=`
@@ -79,8 +83,9 @@ div.innerHTML=`
 
 <div class="row">
 
-<select class="ncc">
+<select class="nccSelect">
 
+<option value="">Chọn NCC</option>
 <option>Cường Vỹ</option>
 <option>Phúc Tiến</option>
 <option>Quang Tuệ</option>
@@ -90,8 +95,11 @@ div.innerHTML=`
 <option>Vân Long</option>
 <option>Phú Hưng</option>
 <option>Thập Hùng</option>
+<option value="khac">Khác...</option>
 
 </select>
+
+<input placeholder="Nhập NCC" class="nccInput" style="display:none">
 
 <select class="logo">
 
@@ -104,24 +112,58 @@ div.innerHTML=`
 </div>
 
 <div class="row">
+
+<input placeholder="Độ dày ván (mm)" class="vanmm" value="${data.vanmm||""}">
+
 <input placeholder="Số lượng" class="soluong" value="${data.soluong||""}">
-<input placeholder="Giá EXW" class="exw" value="${data.exw||""}">
+
 </div>
 
 <div class="row">
+
+<input placeholder="Giá EXW" class="exw" value="${data.exw||""}">
+
 <input placeholder="Chi phí Log" class="log" value="${data.log||""}">
+
+</div>
+
+<div class="row">
+
 <input placeholder="Giá bán" class="ban" value="${data.ban||""}">
+
 </div>
 
 <div class="ketqua">
+
 Tổng: <span class="tong">0</span> |
 Doanh thu: <span class="doanhthu">0</span> |
 Lợi nhuận: <span class="loinhuan">0</span>
+
 </div>
 
 <button class="copy">Copy đơn</button>
 
 `
+
+let select=div.querySelector(".nccSelect")
+let input=div.querySelector(".nccInput")
+
+select.addEventListener("change",()=>{
+
+if(select.value==="khac"){
+
+input.style.display="block"
+
+}else{
+
+input.style.display="none"
+input.value=""
+
+}
+
+luu()
+
+})
 
 div.addEventListener("input",(e)=>{
 
@@ -132,24 +174,31 @@ dinhDangInput(e.target)
 }
 
 tinh(div)
-
 luu()
 
 })
 
 div.querySelector(".copy").onclick=function(){
 
+let ncc=select.value
+
+if(ncc==="khac"){
+ncc=input.value
+}
+
 let text=`
 
-Ngày đặt: ${div.querySelector(".ngaydat").value}
-Ngày giao: ${div.querySelector(".ngaygiao").value}
+Ngày đặt: ${formatDateVN(div.querySelector(".ngaydat").value)}
+Ngày giao: ${formatDateVN(div.querySelector(".ngaygiao").value)}
 
-Nhà cung cấp: ${div.querySelector(".ncc").value}
+Nhà cung cấp: ${ncc}
 Logo: ${div.querySelector(".logo").value}
 
+Độ dày ván: ${div.querySelector(".vanmm").value} mm
 Số lượng: ${div.querySelector(".soluong").value}
+
 Giá EXW: ${div.querySelector(".exw").value}
-Logistics: ${div.querySelector(".log").value}
+Chi phí Log: ${div.querySelector(".log").value}
 Giá bán: ${div.querySelector(".ban").value}
 
 Tổng: ${div.querySelector(".tong").innerText}
@@ -176,10 +225,20 @@ let arr=[]
 
 document.querySelectorAll(".order").forEach(o=>{
 
+let select=o.querySelector(".nccSelect")
+let input=o.querySelector(".nccInput")
+
+let ncc=select.value
+
+if(ncc==="khac"){
+ncc=input.value
+}
+
 arr.push({
 
 ngaydat:o.querySelector(".ngaydat").value,
 ngaygiao:o.querySelector(".ngaygiao").value,
+vanmm:o.querySelector(".vanmm").value,
 soluong:o.querySelector(".soluong").value,
 exw:o.querySelector(".exw").value,
 log:o.querySelector(".log").value,
@@ -217,14 +276,25 @@ taoDon()
 
 function xuatExcel(){
 
-let rows=[["Ngày đặt","Ngày giao","Số lượng","EXW","Log","Giá bán","Lợi nhuận"]]
+let rows=[["Ngày đặt","Ngày giao","Nhà cung cấp","Ván mm","Số lượng","EXW","Log","Giá bán","Lợi nhuận"]]
 
 document.querySelectorAll(".order").forEach(o=>{
 
+let select=o.querySelector(".nccSelect")
+let input=o.querySelector(".nccInput")
+
+let ncc=select.value
+
+if(ncc==="khac"){
+ncc=input.value
+}
+
 rows.push([
 
-o.querySelector(".ngaydat").value,
-o.querySelector(".ngaygiao").value,
+formatDateVN(o.querySelector(".ngaydat").value),
+formatDateVN(o.querySelector(".ngaygiao").value),
+ncc,
+o.querySelector(".vanmm").value,
 o.querySelector(".soluong").value,
 o.querySelector(".exw").value,
 o.querySelector(".log").value,
